da87b767157b897911f169ddbe467526c54a7973
Leviticur <54155983+Leviticur@users.noreply.github.com>
2020-10-06 23:26:05 -0400

    Update opening_explorer.py

--- opening_explorer.py
+++ opening_explorer.py
@@ -35,6 +35,7 @@ else:
 white = True
 opponent = None
 time_control = None
+excluded_opponents = list()
 
 moves = list()
 
@@ -43,6 +44,7 @@ def opening_explorer(jsdata):
     global white
     global opponent
     global time_control
+    global excluded_opponents
     global moves
     if jsdata.startswith("setcolor"):
         white = True if jsdata[8:] == "white" else False
@@ -53,11 +55,16 @@ def opening_explorer(jsdata):
             time_control = None
         else:
             time_control = jsdata[14:]
+    elif jsdata.startswith("setexcludedopponent"):
+        excluded_opponents.append(jsdata[19:])
+    elif jsdata == "resetopponents":
+        opponent = None
+        excluded_opponents = list()
     else:  # If board position is sent
         moves = jsdata.replace("_", " ").split()  # Could split at _
         del moves[0::3]
 
-    games = explorer_functions.get_games(white, username, game_data, moves, time_control, opponent)
+    games = explorer_functions.get_games(white, username, game_data, moves, time_control, opponent, excluded_opponents)
     moves_data = explorer_functions.get_moves_data(games, moves)
 
     ordered_moves_data = sorted(moves_data.items(), key=operator.itemgetter(1))  # sorts responses by moves played
